10aea751b27e340791dbabff01c999d47be599f0
Daniel Olesen <dto@outlook.dk>
2022-05-09 18:02:35 +0200

    updated to run of filterlists.com

--- build.py
+++ build.py
@@ -1,92 +1,131 @@
 import hashlib
-
 import requests
-
-dict = {
-
-    # uBlock
-    "uBlock filters": "https://ublockorigin.github.io/uAssets/filters/filters.txt",
-    "uBlock filters - Badware risks": "https://ublockorigin.github.io/uAssets/filters/badware.txt",
-    "uBlock filters - Privacy": "https://ublockorigin.github.io/uAssets/filters/privacy.txt",
-    "uBlock filters - Quick fixes": "https://ublockorigin.github.io/uAssets/filters/quick-fixes.txt",
-    "uBlock filters - Resource abuse": "https://ublockorigin.github.io/uAssets/filters/resource-abuse.txt",
-    "uBlock filters - Unbreak": "https://ublockorigin.github.io/uAssets/filters/unbreak.txt",
-
-    # Ads
-    "AdGuard Base": "https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt",
-    "AdGuard Mobile Ads": "https://filters.adtidy.org/extension/ublock/filters/11.txt",
-    "EasyList": "https://easylist.to/easylist/easylist.txt",
-
-    # Privacy
-    "AdGuard Tracking Protection": "https://filters.adtidy.org/extension/ublock/filters/3.txt",
-    "AdGuard URL Tracking Protection": "https://filters.adtidy.org/extension/ublock/filters/17.txt",
-    "Block Outsider Intrusion into LAN": "https://ublockorigin.github.io/uAssets/filters/lan-block.txt",
-    "EasyPrivacy": "https://easylist.to/easylist/easyprivacy.txt",
-
-    # Malware domains
-    "Online Malicious URL Blocklist": "https://curben.gitlab.io/malware-filter/urlhaus-filter-online.txt",
-    "Phishing URL Blocklist": "https://curben.gitlab.io/malware-filter/phishing-filter.txt",
-    "PUP Domains Blocklist": "https://curben.gitlab.io/malware-filter/pup-filter.txt",
-
-    # Malware protection
-    "Spam404": "https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt",
-
-    # Anti-circumvention
-    "ABP filters": "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
-
-    # Annoyances
-    "AdGuard Annoyances": "https://filters.adtidy.org/extension/ublock/filters/14.txt",
-    "AdGuard Social Media": "https://filters.adtidy.org/extension/ublock/filters/4.txt",
-    "Anti-Facebook": "https://filters.adtidy.org/extension/ublock/filters/4.txt",
-    "EasyList Cookie": "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
-    "Fanboy’s Annoyance": "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
-    "Fanboy’s Social": "https://easylist.to/easylist/fanboy-social.txt",
-    "uBlock filters – Annoyances": "https://ublockorigin.github.io/uAssets/filters/annoyances.txt",
-
-    # Multi-purpose
-    "Dan Pollock’s hosts file": "https://someonewhocares.org/hosts/hosts",
-    "Peter Lowe’s Ad and tracking server list": "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
-
-    # Other Custom filters
-    "Actually Legitimate URL Shortener Tool": "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt",
-    "0131 Block List": "https://austinhuang.me/0131-block-list/list.txt"
-}
 
 tempFile = "a.temp"
 outFile = "block-all.txt"
 
 def main():
     get_lists()
-    remove_duplicates_and_comments()
+    remove_duplicates()
 
 
 def get_lists():
     print("Downloading lists...")
+
+    response = requests.get(
+        "https://filterlists.com/api/directory/lists",
+        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+        )
+    response.raise_for_status()
+    lists = response.json()
+
     open(tempFile, "w").close()
 
-    for list in dict:
-        print(f"Fetching {list}")
-        f = requests.get(dict[list])
-        with open(tempFile, "a", encoding="utf-8") as file:
-            file.write(f.text.rstrip())
+    for list in lists:
+        if find_maintainer(list["maintainerIds"]) and find_syntax(list["syntaxIds"]) and block_tags(list['tagIds']) and find_license(list["licenseId"]) and find_language(list["languageIds"]):
+            print("Downloading list: " + list["name"])
+            blob = requests.get(list['primaryViewUrl']).text.strip()
+            with open(tempFile, "a", encoding="utf-8") as output_file:
+                output_file.write(blob + "\n")
 
     print("Done!")
 
-def remove_duplicates_and_comments():
-    print("Removing duplicates and comments...")
+def remove_duplicates():
+    print("Removing duplicates...")
 
     completed_lines = set()
 
     with open(outFile, "w", encoding="utf-8") as output_file:
         for lines in open(tempFile, "r", encoding="utf-8"):
             line = lines.rstrip()
-            if not line.startswith(("!", "# ", "#	", "  ", "	", " 	")):
+            if not line.startswith(("!", "# ", "#	", "  ", "	", " 	", "[")):
                 hash_value = hashlib.md5(line.encode('utf-8')).hexdigest()
                 if hash_value not in completed_lines:
                     output_file.write(line + "\n")
                     completed_lines.add(hash_value)
 
     print("Done!")
+
+def find_syntax(list):
+    # https://filterlists.com/api/directory/syntaxes
+
+    for id in list:
+        if id in {
+            3, # Adblock Plus
+            4, # uBlock Origin Static
+            6, # AdGuard
+            17, # uBlock Origin scriptlet injection
+            28, # Adblocker-syntax domains
+            38, # Adblock Plus Advanced
+        }:
+            return True
+
+def find_license(license):
+    # https://filterlists.com/api/directory/licenses
+
+    if license in {
+        2, # The MIT License (MIT)
+        6, # CC BY-NC-SA 3.0
+        8, # CC BY-SA 4.0
+        9, # CC BY-NC-SA 4.0
+        10, # BSD-3-Clause
+        11, # The Unlicense
+        12, # CC BY-SA 3.0
+        13, # CC BY-NC 4.0
+        14, # WTFPL
+        16, # CC BY 3.0
+        17, # ISC
+        18, # Apache 2.0
+        20, # Public Domain
+        23, # "Dont Be a Dick" Public License
+        24, # CC BY 4.0
+        27, # BSD-2-Clause
+        28, # CC0 1.0 Universal
+        32, # Permissive non-commercial
+        33, # Mozilla Public License v2.0
+        35, # Dandelicence
+        37, # Lesser GPL v2.1
+        38, # Generic copyfree
+        40, # McRae General Public License
+        44, # CPAL-1.0
+    }:
+        return True
+
+def find_maintainer(maintainers):
+    # https://filterlists.com/api/directory/maintainers
+
+    for maintainer in maintainers:
+        if maintainer in {
+            7, # The EasyList Authors
+            45, # Disconnect
+            46, # AdGuard
+            82, # AdBlock
+        }:
+            return True
+
+def block_tags(tags):
+    # https://filterlists.com/api/directory/tags
+
+    for tag in tags:
+        if tag in {
+            10, # allowlist
+            38, # beta
+        }:
+            return False
+    return True
+
+def find_language(languages):
+    # https://filterlists.com/api/directory/languages
+
+    if languages == []:
+        return True
+
+    for language in languages:
+        if language in {
+            31, # Danish
+            37, # English
+        }:
+            return True
 
 if __name__ == "__main__":
     main()
