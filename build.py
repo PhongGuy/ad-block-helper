191e24b80127654d231a08c40865447a48439a2f
Daniel Olesen <dto@outlook.dk>
2022-05-20 22:31:49 +0200

    code improvements

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import requests
 tempFile = "a.temp"
 outFile = "block-all.txt"
 
+
 def main():
     get_lists()
     remove_duplicates()
@@ -14,21 +15,25 @@ def get_lists():
 
     response = requests.get(
         "https://filterlists.com/api/directory/lists",
-        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
-        )
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/83.0.4103.116 Safari/537.36 "
+        }
+    )
     response.raise_for_status()
     lists = response.json()
 
     open(tempFile, "w").close()
 
-    for list in lists:
-        if find_maintainer(list["maintainerIds"]) and find_syntax(list["syntaxIds"]) and block_tags(list['tagIds']) and find_license(list["licenseId"]) and find_language(list["languageIds"]):
-            print("Downloading list: " + list["name"])
-            blob = requests.get(list['primaryViewUrl']).text.strip()
+    for ad_filter in lists:
+        if find_maintainer(ad_filter["maintainerIds"]) and find_syntax(ad_filter["syntaxIds"]) and block_tags(ad_filter['tagIds']) and find_license(ad_filter["licenseId"]) and find_language(ad_filter["languageIds"]):
+            print("Downloading list: " + ad_filter["name"])
+            blob = requests.get(ad_filter['primaryViewUrl']).text.strip()
             with open(tempFile, "a", encoding="utf-8") as output_file:
                 output_file.write(blob + "\n")
 
     print("Done!")
+
 
 def remove_duplicates():
     print("Removing duplicates...")
@@ -46,86 +51,91 @@ def remove_duplicates():
 
     print("Done!")
 
-def find_syntax(list):
+
+def find_syntax(ad_filter):
     # https://filterlists.com/api/directory/syntaxes
 
-    for id in list:
-        if id in {
-            3, # Adblock Plus
-            4, # uBlock Origin Static
-            6, # AdGuard
-            17, # uBlock Origin scriptlet injection
-            28, # Adblocker-syntax domains
-            38, # Adblock Plus Advanced
+    for syntax_id in ad_filter:
+        if syntax_id in {
+            3,  # Adblock Plus
+            4,  # uBlock Origin Static
+            6,  # AdGuard
+            17,  # uBlock Origin scriptlet injection
+            28,  # Adblocker-syntax domains
+            38,  # Adblock Plus Advanced
         }:
             return True
 
-def find_license(license):
+
+def find_license(ad_license):
     # https://filterlists.com/api/directory/licenses
 
-    if license in {
-        2, # The MIT License (MIT)
-        6, # CC BY-NC-SA 3.0
-        8, # CC BY-SA 4.0
-        9, # CC BY-NC-SA 4.0
-        10, # BSD-3-Clause
-        11, # The Unlicense
-        12, # CC BY-SA 3.0
-        13, # CC BY-NC 4.0
-        14, # WTFPL
-        16, # CC BY 3.0
-        17, # ISC
-        18, # Apache 2.0
-        20, # Public Domain
-        23, # "Dont Be a Dick" Public License
-        24, # CC BY 4.0
-        27, # BSD-2-Clause
-        28, # CC0 1.0 Universal
-        32, # Permissive non-commercial
-        33, # Mozilla Public License v2.0
-        35, # Dandelicence
-        37, # Lesser GPL v2.1
-        38, # Generic copyfree
-        40, # McRae General Public License
-        44, # CPAL-1.0
+    if ad_license in {
+        2,  # The MIT License (MIT)
+        6,  # CC BY-NC-SA 3.0
+        8,  # CC BY-SA 4.0
+        9,  # CC BY-NC-SA 4.0
+        10,  # BSD-3-Clause
+        11,  # The Unlicensed
+        12,  # CC BY-SA 3.0
+        13,  # CC BY-NC 4.0
+        14,  # WTFPL
+        16,  # CC BY 3.0
+        17,  # ISC
+        18,  # Apache 2.0
+        20,  # Public Domain
+        23,  # "Don't Be a Dick" Public License
+        24,  # CC BY 4.0
+        27,  # BSD-2-Clause
+        28,  # CC0 1.0 Universal
+        32,  # Permissive non-commercial
+        33,  # Mozilla Public License v2.0
+        35,  # Dandelicence
+        37,  # Lesser GPL v2.1
+        38,  # Generic copyfree
+        40,  # McRae General Public License
+        44,  # CPAL-1.0
     }:
         return True
+
 
 def find_maintainer(maintainers):
     # https://filterlists.com/api/directory/maintainers
 
     for maintainer in maintainers:
         if maintainer in {
-            7, # The EasyList Authors
-            45, # Disconnect
-            46, # AdGuard
-            82, # AdBlock
+            7,  # The EasyList Authors
+            45,  # Disconnect
+            46,  # AdGuard
+            82,  # AdBlock
         }:
             return True
+
 
 def block_tags(tags):
     # https://filterlists.com/api/directory/tags
 
     for tag in tags:
         if tag in {
-            10, # allowlist
-            38, # beta
+            10,  # allow-list
+            38,  # beta
         }:
             return False
     return True
 
+
 def find_language(languages):
     # https://filterlists.com/api/directory/languages
 
-    if languages == []:
-        return True
+    if languages:
+        for language in languages:
+            if language not in {
+                31,  # Danish
+                37,  # English
+            }:
+                return False
+    return True
 
-    for language in languages:
-        if language in {
-            31, # Danish
-            37, # English
-        }:
-            return True
 
 if __name__ == "__main__":
     main()
